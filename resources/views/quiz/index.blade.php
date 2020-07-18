7ef75f10ea8bcd71e052330fa6df3aeae12f2471
s <>
2020-07-18 22:50:43 +0900

    twitter共有ボタン２

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -9,7 +9,7 @@
         <input class="p-detail__link" type="text" readonly="readonly"
                value="https://share-with-friends-a.herokuapp.com/quiz/test/{{ $problem->id }}">
         <a class="twitter-share-button"
-           href="https://twitter.com/intent/tweet?text=https://share-with-friends-a.herokuapp.com/quiz/test/{{ $problem->id }}">
+           href="https://twitter.com/intent/tweet?text=クイズを作ったのでやってみてください!!%20https://share-with-friends-a.herokuapp.com/quiz/test/{{ $problem->id }}">
             Tweetで共有する</a>
     </div>
     <p class="p-detail__subject u-center">クイズテーマ　：　{{ $problem->subject }}</p>
